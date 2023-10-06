62e0e410749b08d2d17d69e853025f89cfba2bbb
CAPTAIN-JOB <job3kiptoo@gmail.com>
2023-10-06 16:46:05 +0300

    router

--- app/components/navBar/page.tsx
+++ app/components/navBar/page.tsx
@@ -3,10 +3,10 @@ export default function Nav() {
   return (
     <nav className="flex sm:justify-center space-x-4">
       {[
-        ['Home', '/dashboard'],
-        ['About', '/About'],
-        ['Projects', '/projects'],
-        ['Contact', '/Contact'],
+        ['Home', '/'],
+        ['About', '/'],
+        ['Projects', '/'],
+        ['Contact', '/'],
       ].map(([title, url]) => (
         <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
       ))}
